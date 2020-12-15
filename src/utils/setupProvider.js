8d80a39e17c07fc62b17f1107012bd999bff5091
justin j. moses <justinjmoses@gmail.com>
2020-12-15 14:29:31 -0500

    Fix setupProvider for condition where no pk

--- src/utils/setupProvider.js
+++ src/utils/setupProvider.js
@@ -12,13 +12,13 @@ function setupProvider({ providerUrl, privateKey, publicKey }) {
 	if (publicKey) {
 		wallet = provider.getSigner(publicKey);
 		wallet.address = publicKey;
-	} else if (privateKey) {
+	} else {
 		wallet = new ethers.Wallet(privateKey || ethers.Wallet.createRandom().privateKey, provider);
 	}
 
 	return {
 		provider,
-		wallet: wallet || undefined,
+		wallet,
 	};
 }
 
