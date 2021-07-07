10c9b9754249235c5f0fefbae4058b42bf80a838
Alejandro <palebluedot@gmail.com>
2021-07-07 11:55:09 -0300

    Improvements to airdrop script

--- src/commands/get-all-l2-active-snx-holders.js
+++ src/commands/get-all-l2-active-snx-holders.js
@@ -9,6 +9,150 @@ const { getPastEvents } = require('../utils/getEvents');
 const { getContract } = require('../utils/getContract');
 const { wrap } = require('synthetix');
 
+// There are currently 2 bridge versions: 0x045e507925d2e05D114534D0810a1abD94aca8d6, and 0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068
+// https://etherscan.io/address/0x045e507925d2e05D114534D0810a1abD94aca8d6#events
+// https://etherscan.io/address/0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068#events
+const knownBridges = {
+	'0x045e507925d2e05D114534D0810a1abD94aca8d6': {
+		fromBlock: 11656238,
+		depositEventName: 'Deposit',
+		depositEventTarget: 'account',
+		migrateEventName: 'ExportedVestingEntries',
+		migrateEventTarget: 'account',
+		abi: [
+			{
+				"anonymous":false,
+				"inputs":[
+					 {
+							"indexed":true,
+							"internalType":"address",
+							"name":"account",
+							"type":"address"
+					 },
+					 {
+							"indexed":false,
+							"internalType":"uint256",
+							"name":"amount",
+							"type":"uint256"
+					 }
+				],
+				"name":"Deposit",
+				"type":"event"
+			},
+			{
+				"anonymous":false,
+				"inputs":[
+					 {
+							"indexed":true,
+							"internalType":"address",
+							"name":"account",
+							"type":"address"
+					 },
+					 {
+							"indexed":false,
+							"internalType":"uint256",
+							"name":"escrowedAccountBalance",
+							"type":"uint256"
+					 },
+					 {
+							"components":[
+								 {
+										"internalType":"uint64",
+										"name":"endTime",
+										"type":"uint64"
+								 },
+								 {
+										"internalType":"uint256",
+										"name":"escrowAmount",
+										"type":"uint256"
+								 }
+							],
+							"indexed":false,
+							"internalType":"struct VestingEntries.VestingEntry[]",
+							"name":"vestingEntries",
+							"type":"tuple[]"
+					 }
+				],
+				"name":"ExportedVestingEntries",
+				"type":"event"
+			},
+		],
+	},
+	'0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068': {
+		fromBlock: 12409013,
+		depositEventName: 'DepositInitiated',
+		depositEventTarget: '_to',
+		migrateEventName: 'ExportedVestingEntries',
+		migrateEventTarget: 'account',
+		abi: [
+			{
+				"anonymous":false,
+				"inputs":[
+					 {
+							"indexed":true,
+							"internalType":"address",
+							"name":"_from",
+							"type":"address"
+					 },
+					 {
+							"indexed":false,
+							"internalType":"address",
+							"name":"_to",
+							"type":"address"
+					 },
+					 {
+							"indexed":false,
+							"internalType":"uint256",
+							"name":"_amount",
+							"type":"uint256"
+					 }
+				],
+				"name":"DepositInitiated",
+				"type":"event"
+			},
+			{
+				"anonymous":false,
+				"inputs":[
+					 {
+							"indexed":true,
+							"internalType":"address",
+							"name":"account",
+							"type":"address"
+					 },
+					 {
+							"indexed":false,
+							"internalType":"uint256",
+							"name":"escrowedAccountBalance",
+							"type":"uint256"
+					 },
+					 {
+							"components":[
+								 {
+										"internalType":"uint64",
+										"name":"endTime",
+										"type":"uint64"
+								 },
+								 {
+										"internalType":"uint256",
+										"name":"escrowAmount",
+										"type":"uint256"
+								 }
+							],
+							"indexed":false,
+							"internalType":"struct VestingEntries.VestingEntry[]",
+							"name":"vestingEntries",
+							"type":"tuple[]"
+					 }
+				],
+				"name":"ExportedVestingEntries",
+				"type":"event"
+			},
+		],
+	},
+};
+
+const network = 'mainnet';
+
 async function getAllActiveSnxHolders({
 	providerUrlL1,
 	providerUrlL2,
@@ -16,8 +160,6 @@ async function getAllActiveSnxHolders({
 	toBlock,
 	clear,
 }) {
-	const network = 'mainnet';
-
 	// Validate input parameters
 	if (!providerUrlL1) throw new Error('Please specify a provider');
 	if (!providerUrlL2) throw new Error('Please specify a provider');
@@ -41,11 +183,105 @@ async function getAllActiveSnxHolders({
 		}
 	}
 
-	const addressesThatDeposited = await _getAllAddressesThatDepositedOnL1({ network, data, toBlock, dataFile, providerUrl: providerUrlL1 });
-  await _getBalancesOnL2({ network, data, dataFile, candidates: addressesThatDeposited, providerUrl: providerUrlL2 });
+	_verifyKnownBridges();
+
+	const candidates = await _getAllAddressesThatDepositedOrMigratedEscrowOnL1({ data, toBlock, dataFile, providerUrl: providerUrlL1 });
+  await _getBalancesOnL2({ data, dataFile, candidates, providerUrl: providerUrlL2 });
 }
 
-async function _getBalancesOnL2({ data, dataFile, network, candidates, providerUrl }) {
+async function _getAllAddressesThatDepositedOrMigratedEscrowOnL1({ toBlock, data, dataFile, providerUrl }) {
+	console.log(chalk.blue(`> Getting all addresses that triggered Deposit or DepositInitiated events in L1 with provider ${providerUrl}`));
+
+	const provider = _getProvider({ providerUrl });
+
+	const knownAddresses = Object.keys(knownBridges);
+
+	const addresses = [];
+	for (const bridgeAddress of knownAddresses) {
+		const bridge = knownBridges[bridgeAddress];
+		const { fromBlock, depositEventName, depositEventTarget, migrateEventName, migrateEventTarget, abi } = bridge;
+
+		const contract = new ethers.Contract(bridgeAddress, abi, provider);
+
+		// Build filter to look for logs
+		// See: https://docs.ethers.io/v5/single-page/#/v5/concepts/events/-%23-events--filters
+		// Notice the double array in topics. This means depositEventName OR migrateEventName
+		console.log(chalk.gray(`  > Looking for ${depositEventName} and ${migrateEventName} events in bridge at ${bridgeAddress} from block ${fromBlock}`));
+		const filter = {
+			address: contract.address,
+			topics: [_getEventTopics({ contract, eventNames: [depositEventName, migrateEventName] })],
+			fromBlock: fromBlock,
+			toBlock: !isNaN(toBlock) ? +toBlock : 'latest',
+		};
+		console.log(chalk.gray(`  > Looking for events from block ${filter.fromBlock} to block ${filter.toBlock}`));
+
+		// Find all events from this bridge
+		const logs = await provider.getLogs(filter);
+		const events = _parseLogs({ contract, logs });
+
+		// Split event types
+		const depositEvents = events.filter(e => e.name === depositEventName);
+		console.log(chalk.gray(`    * found ${depositEvents.length} ${depositEventName} events`));
+		// Discard migration events whose tx hash exists in a deposit event
+		const migrateEvents = events.filter(e => e.name === migrateEventName)
+			.filter(m => !depositEvents.some(d => d.transactionHash === m.transactionHash));
+		console.log(chalk.gray(`    * found ${migrateEvents.length} ${migrateEventName} events`));
+
+		depositEvents.map(event => {
+			const address = event.args[depositEventTarget].toLowerCase();
+			if (!addresses.includes(address)) {
+				addresses.push(address);
+			}
+		});
+
+		migrateEvents.map(event => {
+			const address = event.args[migrateEventTarget].toLowerCase();
+			if (!addresses.includes(address)) {
+				addresses.push(address);
+			}
+		});
+	}
+	console.log(chalk.gray(`  * Found ${addresses.length} unique addresses`));
+
+	data.depositors = addresses.length;
+	fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
+
+	return addresses;
+}
+
+function _getEventTopics({ contract, eventNames }) {
+	const allTopics = [];
+
+	for (eventName of eventNames) {
+		const topics = contract.filters[eventName]().topics;
+		allTopics.push(...topics);
+	}
+
+	return allTopics;
+}
+
+function _verifyKnownBridges() {
+	console.log(chalk.blue(`> Verifying known bridges`));
+	const knownAddresses = Object.keys(knownBridges);
+	console.log(chalk.gray(`  * Expecting ${knownAddresses.length} bridge versions with addresses: ${knownAddresses}`));
+
+	// Recorded versions should be the ones we expect
+	const { getVersions } = wrap({ network, useOvm: false, fs, path });
+	const { SynthetixBridgeToOptimism: registeredBridges } = getVersions({ byContract: true, fs, path })
+	const registeredAddresses = registeredBridges.map(b => b.address);
+	console.log(chalk.gray(`  * Found ${registeredAddresses.length} bridge versions with addresses: ${registeredAddresses}`));
+	if (registeredAddresses.toString() !== knownAddresses.toString()) {
+		throw new Error('Mismatching between registered and expected bridges');
+	}
+}
+
+function _parseLogs({ contract, logs }) {
+	return logs.map(log =>
+		Object.assign({ transactionHash: log.transactionHash, logIndex: log.logIndex }, contract.interface.parseLog(log)),
+	);
+}
+
+async function _getBalancesOnL2({ data, dataFile, candidates, providerUrl }) {
 	console.log(chalk.blue(`> Getting addresses balances on L2 with provider ${providerUrl}`));
 
 	const provider = _getProvider({ providerUrl });
@@ -100,125 +336,6 @@ function _getProvider({ providerUrl }) {
 	} else {
 		return new ethers.getDefaultProvider();
 	}
-}
-
-async function _getAllAddressesThatDepositedOnL1({ network, toBlock, data, dataFile, providerUrl }) {
-	console.log(chalk.blue(`> Getting all L1 Deposit events in provider ${providerUrl}`));
-
-	const provider = _getProvider({ providerUrl });
-
-	// There are currently 2 bridge versions: 0x045e507925d2e05D114534D0810a1abD94aca8d6, and 0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068
-	// https://etherscan.io/address/0x045e507925d2e05D114534D0810a1abD94aca8d6#events
-	// https://etherscan.io/address/0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068#events
-	const knownBridges = {
-		'0x045e507925d2e05D114534D0810a1abD94aca8d6': {
-			fromBlock: 11656238,
-			eventName: 'Deposit',
-			eventTarget: 'account',
-			abi: [
-				{
-					"anonymous":false,
-					"inputs":[
-						 {
-								"indexed":true,
-								"internalType":"address",
-								"name":"account",
-								"type":"address"
-						 },
-						 {
-								"indexed":false,
-								"internalType":"uint256",
-								"name":"amount",
-								"type":"uint256"
-						 }
-					],
-					"name":"Deposit",
-					"type":"event"
-			 }
-			],
-		},
-		'0xCd9D4988C0AE61887B075bA77f08cbFAd2b65068': {
-			fromBlock: 12409013,
-			eventName: 'DepositInitiated',
-			eventTarget: '_to',
-			abi: [
-				{
-					"anonymous":false,
-					"inputs":[
-						 {
-								"indexed":true,
-								"internalType":"address",
-								"name":"_from",
-								"type":"address"
-						 },
-						 {
-								"indexed":false,
-								"internalType":"address",
-								"name":"_to",
-								"type":"address"
-						 },
-						 {
-								"indexed":false,
-								"internalType":"uint256",
-								"name":"_amount",
-								"type":"uint256"
-						 }
-					],
-					"name":"DepositInitiated",
-					"type":"event"
-				}
-			],
-		},
-	};
-	const knownAddresses = Object.keys(knownBridges);
-	console.log(chalk.gray(`  * Expecting ${knownAddresses.length} bridge versions with addresses: ${knownAddresses}`));
-
-	// Recorded versions should be the ones we expect
-	const { getVersions } = wrap({ network, useOvm: false, fs, path });
-	const { SynthetixBridgeToOptimism: registeredBridges } = getVersions({ byContract: true, fs, path })
-	const registeredAddresses = registeredBridges.map(b => b.address);
-	console.log(chalk.gray(`  * Found ${registeredAddresses.length} bridge versions with addresses: ${registeredAddresses}`));
-	if (registeredAddresses.toString() !== knownAddresses.toString()) {
-		throw new Error('Mismatching between registered and expected bridges');
-	}
-
-	const addresses = [];
-	for (const bridgeAddress of knownAddresses) {
-		const bridge = knownBridges[bridgeAddress];
-		const { fromBlock, eventName, eventTarget, abi } = bridge;
-
-		// Find deposit events
-		const contract = new ethers.Contract(bridgeAddress, abi, provider);
-		console.log(chalk.gray(`  > Looking for ${eventName} events in bridge at ${bridgeAddress} from block ${fromBlock}`));
-
-		// Build filter to look for logs
-		const filter = contract.filters[eventName]();
-		filter.fromBlock = fromBlock;
-		filter.toBlock = !isNaN(toBlock) ? +toBlock : 'latest';
-		console.log(chalk.yellow.bold(`>>> Looking for events from block ${filter.fromBlock} to block ${filter.toBlock} <<<`));
-
-		// Find logs
-		const logs = await provider.getLogs(filter);
-		const events = logs.map(log =>
-			Object.assign({ transactionHash: log.transactionHash, logIndex: log.logIndex }, contract.interface.parseLog(log)),
-		);
-		console.log(chalk.gray(`    * found ${events.length} ${eventName} events`));
-
-		// Get all deposit addresses
-		for (event of events) {
-			const depositAddress = event.args[eventTarget].toLowerCase();
-
-			if (!addresses.includes(depositAddress)) {
-				addresses.push(depositAddress);
-			}
-		}
-	}
-	console.log(chalk.gray(`  * Found ${addresses.length} unique addresses that deposited on L1`));
-
-	data.depositors = addresses.length;
-	fs.writeFileSync(dataFile, JSON.stringify(data, null, 2));
-
-	return addresses;
 }
 
 program
