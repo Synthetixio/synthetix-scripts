d630c8707aea30ff3645311dc477c40f3ddc1363
Leonardo Massazza <lmassazza+github@gmail.com>
2021-08-19 10:17:15 -0500

    Add monitor script (post execution)

--- src/utils/getEvents.js
+++ src/utils/getEvents.js
@@ -1,6 +1,27 @@
 const { gray } = require('chalk');
 
 async function getPastEvents({ contract, eventName, provider, fromBlock, toBlock }) {
+	const filter = getFilter({ contract, eventName, fromBlock, toBlock })
+	let logs = await provider.getLogs(filter);
+	logs = logs.map(log =>
+		Object.assign({ transactionHash: log.transactionHash, logIndex: log.logIndex }, contract.interface.parseLog(log)),
+	);
+
+	return logs;
+}
+
+async function waitForEvent({ contract, eventName, provider, fromBlock, toBlock, validator, timeout }) {
+
+	const filter = getFilter({ contract, eventName, fromBlock, toBlock })
+	let logs = await provider.getLogs(filter);
+	logs = logs.map(log =>
+		Object.assign({ transactionHash: log.transactionHash, logIndex: log.logIndex }, contract.interface.parseLog(log)),
+	);
+
+	return logs;
+}
+
+function getFilter({ contract, eventName, fromBlock, toBlock, listeningMode }) {
 	let filter = { address: contract.address };
 
 	if (eventName) {
@@ -8,15 +29,12 @@ async function getPastEvents({ contract, eventName, provider, fromBlock, toBlock
 		if (!filter) throw new Error(`Event ${eventName} not found in contract abi.`);
 	}
 
-	filter.fromBlock = +fromBlock || 'earliest';
-	filter.toBlock = +toBlock || 'latest';
+	if (!listeningMode) {
+		filter.fromBlock = +fromBlock || 'earliest';
+		filter.toBlock = +toBlock || 'latest';
+	}
 
-	let logs = await provider.getLogs(filter);
-	logs = logs.map(log =>
-		Object.assign({ transactionHash: log.transactionHash, logIndex: log.logIndex }, contract.interface.parseLog(log)),
-	);
-
-	return logs;
+	return filter;
 }
 
 module.exports = {
